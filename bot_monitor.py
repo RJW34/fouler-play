23cf2ebd0038efcc89d2fb94fff053f8dcb41dec
DEKU <Dekubot6967@gmail.com>
2026-01-30 16:55:30 -0500

    Switch bot_monitor.py to LEBOTJAMESXD002 for fresh ELO climb

--- bot_monitor.py
+++ bot_monitor.py
@@ -220,7 +220,7 @@ class BotMonitor:
         cmd = [
             "venv/bin/python", "-u", "run.py",  # -u for unbuffered output
             "--websocket-uri", "wss://sim3.psim.us/showdown/websocket",
-            "--ps-username", "LEBOTJAMESXD001",
+            "--ps-username", "LEBOTJAMESXD002",
             "--ps-password", "LeBotPassword2026!",
             "--bot-mode", "search_ladder",
             "--pokemon-format", "gen9ou",
